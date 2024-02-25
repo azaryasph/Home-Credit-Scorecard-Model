49d94b0a7f3de265c296dac35f3156447a1c9ef4
azaryayehezkiel <azaryaemc@gmail.com>
2024-02-25 15:32:52 +0700

    chore: initialize modules folder

---
+++ modules/missing.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_missing_percentage(df):
+    """
+    Plot the percentage of missing values for each column in a DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame to process.
+    """
+    # Calculate the percentage of missing values
+    missing = (df.isnull().sum() / len(df)) * 100
+
+    # Create a DataFrame with the results
+    percentage_missing = pd.DataFrame({'column': df.columns, 'missing_percentage %': missing.values})
+    percentage_missing['missing_percentage %'] = percentage_missing['missing_percentage %'].round(2)
+    percentage_missing = percentage_missing.sort_values('missing_percentage %', ascending=False)
+    percentage_missing = percentage_missing.reset_index()
+    percentage_missing = percentage_missing.drop('index', axis=1)
+
+    # Plot the results
+    plt.figure(figsize=(12, 5))
+    ax = sns.barplot(x='missing_percentage %', y='column', data=percentage_missing, color='#E1341E')
+    for p in ax.patches:
+        ax.annotate("%.2f" % p.get_width() + '%', xy=(p.get_width(), p.get_y()+p.get_height()/2),
+                    xytext=(8, 0), textcoords='offset points', ha="left", va="center", fontsize=10)
+    plt.title('Missing values Percentage for Each Column', fontsize=17, fontweight='bold')
+    plt.ylabel('Kolom', fontsize=12)
+    plt.xlabel('missing_percentage %', fontsize=12)
+    plt.tight_layout()
+    plt.xlim(0, 100)
+    plt.show()
+
+
+    # function to drop columns with missing values
+def dropnan(df, threshold):
+    '''
+    Drop columns from a DataFrame where the proprortion of missing values is greater than specified threshold.
+
+    Parameters:
+    df: The DataFrame to process
+    threshold: The threshold proportion of missing vales. Columns with a proportion of missing values greater than this will be dropped.
+
+    Returns:
+    Pandas.DataFrame: The processed DataFrame. 
+    '''
+    for column in df.columns:
+        missing_proportion = df[column].isnull().sum() / len(df)
+        if missing_proportion > threshold:
+            df = df.drop(column, axis=1)
+    return df
