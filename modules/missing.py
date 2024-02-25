c146e34f80c56d193be00cbf0e352a52ee1938e5
azaryayehezkiel <azaryaemc@gmail.com>
2024-02-25 19:42:02 +0700

    chore: adjust modules for better usage

--- modules/missing.py
+++ modules/missing.py
@@ -15,16 +15,19 @@ def plot_missing_percentage(df):
     # Create a DataFrame with the results
     percentage_missing = pd.DataFrame({'column': df.columns, 'missing_percentage %': missing.values})
     percentage_missing['missing_percentage %'] = percentage_missing['missing_percentage %'].round(2)
+    percentage_missing = percentage_missing[percentage_missing['missing_percentage %'] > 0]
     percentage_missing = percentage_missing.sort_values('missing_percentage %', ascending=False)
     percentage_missing = percentage_missing.reset_index()
     percentage_missing = percentage_missing.drop('index', axis=1)
 
     # Plot the results
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=(12, 9))
     ax = sns.barplot(x='missing_percentage %', y='column', data=percentage_missing, color='#E1341E')
+    
     for p in ax.patches:
         ax.annotate("%.2f" % p.get_width() + '%', xy=(p.get_width(), p.get_y()+p.get_height()/2),
                     xytext=(8, 0), textcoords='offset points', ha="left", va="center", fontsize=10)
+        
     plt.title('Missing values Percentage for Each Column', fontsize=17, fontweight='bold')
     plt.ylabel('Kolom', fontsize=12)
     plt.xlabel('missing_percentage %', fontsize=12)
